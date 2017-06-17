8f876305285c0c4b8619a0c2b1fdae95365ba0fb
Tom Wilson <tom@jackhq.com>
2017-06-17 06:40:46 -0400

    create form

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Button from '../components/button'
 
 const Form = () => {
   return (
-    <div>
-      Form
+    <div className="padding-medium">
+      <h2>Video Form</h2>
+      <form>
+        <div className="measure">
+          <label className="f6 fontweight-bold display-block marginbottom-xxsmall">
+            Name
+          </label>
+          <input
+            className="input-reset border bordercolor-black20 padding-xxsmall marginbottom-xxsmall"
+            type="text"
+          />
+          <small className="f6 black60 display-block marginbottom-xxsmall">
+            Enter short description of video
+          </small>
+        </div>
+        <div>
+          <Button>Submit</Button>
+        </div>
+      </form>
     </div>
   )
 }
