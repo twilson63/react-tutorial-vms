74dfbd089cf39ec59dfe04649e221f448abefae8
Tom Wilson <tom@jackhq.com>
2017-06-17 06:47:28 -0400

    Created TextField Component

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import Button from '../components/button'
+import TextField from '../components/text-field'
 
 const Form = () => {
   return (
     <div className="padding-medium">
       <h2>Video Form</h2>
       <form>
-        <div className="measure">
-          <label className="f6 fontweight-bold display-block marginbottom-xxsmall">
-            Name
-          </label>
-          <input
-            className="input-reset border bordercolor-black20 padding-xxsmall marginbottom-xxsmall"
-            type="text"
-          />
-          <small className="f6 black60 display-block marginbottom-xxsmall">
-            Enter short description of video
-          </small>
-        </div>
+        <TextField label="Name" description="Enter short name of video" />
         <div>
           <Button>Submit</Button>
         </div>
